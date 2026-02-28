4ffbbb7a5fa06c44784da3a598fd872ebab56a66
Harshit <harshit.singh22082007@gmail.com>
2026-02-28 15:39:40 +0530

    docs: Add project README with architecture and diagrams

--- server/routes/demo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const demoController = require('../controllers/demoController');
-
-router.post('/simulate', demoController.simulate);
-router.post('/reset', demoController.reset);
-
-module.exports = router;
